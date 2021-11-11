78ce736821df38bc3e70f30d7630bbec0c0503b9
miniwisdom5999 <miniwisdom@naver.com>
2021-11-11 12:46:45 +0900

    day04

---
+++ seats/js/seats.js
@@ -0,0 +1,34 @@
+ //입장객 수에 따른 좌석의 줄 수 계산하기
+        //변수 생성
+        var customerNum = prompt("입장객은 몇 명인가요?");  //입장객
+        var columnNum = prompt("한 줄에 몇 명 앉나요?");     //열
+        var rowNum = 0;        //줄(행)
+
+        // 처리 : 좌석 줄(행) 수 = 입장객 / 좌석 열 수
+
+        // rowNum = customerNum / columnNum;
+
+        if(customerNum % columnNum == 0){ //나누어 떨어지면
+            rowNum = customerNum / columnNum;  //5개씩 하라는 내용은 여기있음!!!
+        }
+        else{ //나머지가 있으면
+            rowNum = parseInt (customerNum / columnNum) + 1;
+        }
+        //parseInt() - 정수로 변환하는 함수
+
+        //출력
+        // document.write(rowNum + "개의 줄이 필요합니다.");
+
+        //중첩 for로 자리 배치
+        document.write("<table>");
+        for(var i = 0; i < rowNum; i++){
+            document.write("<tr>");
+            for(var j = 1; j <= columnNum; j++){
+                var seatNum = i * columnNum + j; //예 입장객(33) 자리(33)
+                if(seatNum > customerNum) break;
+                document.write("<td>좌석" + seatNum + "</td>");
+                
+            }
+            document.write("</tr>");
+        }
+        document.write("</table>");
